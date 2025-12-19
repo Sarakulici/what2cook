41fb67cea7010ea9dcfbdeb87a2653852ecf64b9
Sarakulici <sara.kulici@mng.ch>
2025-12-19 09:18:21 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -11,3 +11,30 @@ CREATE TABLE todos (
     due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE rezpet (
+    rezept.id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    rezeptname VARCHAR(100),
+    kochanleitung VARCHAR(100),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE zutaten (
+    zutaten.id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    zutatname VARCHAR(100),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE user (
+    user.id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    email VARCHAR(100),
+    passwort VARCHAR(100),
+    benutzername VARCHAR(100),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
