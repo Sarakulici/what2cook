2b144e0d50f2afd14d806d0a6b77b8d2728256d1
Sarakulici <sara.kulici@mng.ch>
2025-12-22 15:31:18 +0100

    Add user table and remove duplicate definition
    Removed duplicate user table definition and added a new user table with foreign key.

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -4,6 +4,16 @@ CREATE TABLE users (
     password VARCHAR(250) NOT NULL
 );
 
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    email VARCHAR(100),
+    passwort VARCHAR(100),
+    benutzername VARCHAR(100),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 CREATE TABLE todos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -29,12 +39,3 @@ CREATE TABLE zutaten (
     FOREIGN KEY (rezept_id) REFERENCES rezept(id)
 );
 
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    email VARCHAR(100),
-    passwort VARCHAR(100),
-    benutzername VARCHAR(100),
-
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
