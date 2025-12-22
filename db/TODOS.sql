4a3027e103f1ea09efd11308b1eac70c351f401d
Sarakulici <sara.kulici@mng.ch>
2025-12-22 15:35:32 +0100

    Rename user_id to users_id in database schema

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,34 +1,29 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(250) NOT NULL UNIQUE,
-    password VARCHAR(250) NOT NULL
-);
-
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    users_id INT NOT NULL,
     email VARCHAR(100),
     passwort VARCHAR(100),
     benutzername VARCHAR(100),
-
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    
+    FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
 CREATE TABLE todos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    users_id INT NOT NULL,
     content VARCHAR(100),
     due DATETIME,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    
+    FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
 CREATE TABLE rezpet (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    users_id INT NOT NULL,
     rezeptname VARCHAR(100),
     kochanleitung VARCHAR(100),
 
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
 CREATE TABLE zutaten (
