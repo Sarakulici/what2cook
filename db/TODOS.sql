062776c0e891448a5242f2340b744b6ba8eca891
Sarakulici <sara.kulici@mng.ch>
2025-12-22 21:40:35 +0100

    Fix ingredient names and add missing values (das wären die Zutaten)

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -55,12 +55,12 @@ VALUES
     (3, 'Tomatensauce', 'Tomaten waschen, ein Kreuz mit dem Messer in die Tomaten schnitzen, 
     die Tomaten für 5 min in kochendes Wasser geben, Tomaten aus dem Wasser sieben und auskühlen lassen, 
     die Schale der Tomaten entfernen und in Stücke schneiden, 
-    Bisschen Öl in die Pfanne geben mit Knoblauch und gehackten Zwiebeln, 
+    Bisschen Olivenöl in die Pfanne geben mit Knoblauch und gehackten Zwiebeln, 
     Sobald es heiss wird die Tomatenstücke hinzufügen und köcheln, 
     entweder mit einem Pürrirstab oder mit einer Gabel die Stücke klein hacken, 20 min köcheln lassen,
     mit Salz, Pfeffern und Kräuter würzen') 
 
-    Tomaten, Öl, Knoblauch, Zwiebel
+    Tomaten, Olivenöl, Knoblauch, Zwiebel
 
     (4, 'toms Erbermarmeladenbrot mit Honig', 'Brotscheibe mit Erdbeermarmelade bestreichen, etwas Honig in die Mitte setzen')
 
@@ -77,5 +77,10 @@ VALUES
     (7, 'Bruschetta')
     (8, 'tortilla de patatas')
     (9, 'Bratkartoffeln')
+;
 
-    ;
+INSERT INTO zutaten (zutaten_id, zutatname, rezept_id)
+VALUES (1, 'Mehl', 1), (2, 'Salz', 1), (3, 'Olivenöl', 1 und 3), (4, 'Hefe', 1), (5, 'Tomatensauce', 1), 
+(6, 'Spaghetti', 2), (7, 'Butter', 2), (8, 'Tomaten', 3), (9, 'Knoblauch', 3), (10, 'Zwiebel', 3), (11, 'Brot', 4), 
+(12, 'Erdbeermarmelade', 4), (13, 'Honig', 4)
+;
